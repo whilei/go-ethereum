63596f27493dc4a8f8b7e8ba9de79bbce9193595
ia <isaac.ardis@gmail.com>
2017-07-17 09:23:54 -0500

    test revised deploy scripts with appveyor too

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir ./deploy
-mv ./dist/*.tar.gz ./dist/*.zip ./deploy/
-#janus deploy -to="builds.etcdevteam.com/go-ethereum/$(janus version -format %M.%m.x)" -files="./deploy/*" -key="./gcloud-travis.json.enc"
-janus deploy -to="builds.etcdevteam.com/go-ethereum/$(janus version -format %M.%m.x)" -files="./deploy/*" -key="./isaac-test-gcp-service-key.enc.json"
+GETH_ARCHIVE_NAME="geth-classic-$TRAVIS_OS_NAME-$(janus version -format='TAG_OR_NIGHTLY')"
+zip "$GETH_ARCHIVE_NAME.zip" geth
+tar -zcf "$GETH_ARCHIVE_NAME.tar.gz" geth
+
+mkdir deploy
+mv *.zip *.tar.gz deploy/
+
+janus deploy -to="builds.etcdevteam.com/go-ethereum/$(janus version -format='v%M.%m.x')" -files="./deploy/*" -key="./gcloud-travis.json.enc"
