a5f0cd43e34f8386cd94d34ed8660e0dca4a6653
ia <isaac.ardis@gmail.com>
2017-12-01 07:56:57 -0600

    Revert "problem: D logging won't see existing logged errors"
    This reverts commit 897c8420ecdd812b3816f80a363b41156e963461.

--- logger/glog/glog.go
+++ logger/glog/glog.go
@@ -1288,8 +1288,6 @@ func (v Verbose) Errorln(args ...interface{}) {
 	if v {
 		logging.println(errorLog, args...)
 	}
-	// always print errors to display
-	display.println(errorLog, args...)
 }
 
 // Errorf is equivalent to the global Errorf function, guarded by the value of v.
@@ -1298,7 +1296,6 @@ func (v Verbose) Errorf(format string, args ...interface{}) {
 	if v {
 		logging.printfmt(errorLog, format, args...)
 	}
-	display.printfmt(errorLog, format, args...)
 }
 
 // Separator creates a line, ie ---------------------------------
