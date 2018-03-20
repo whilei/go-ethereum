4adf0f63eb978b2622fa6f6ed50aa0991bfec26e
ia <isaac.ardis@gmail.com>
2018-03-20 17:45:04 -0500

    problem: should test distinctNetSet with >1 address
    solution: create test for dynamic number of ips
    
    problem: test fails

--- p2p/distip/net_test.go
+++ p2p/distip/net_test.go
@@ -1,39 +1,73 @@
 package distip
 
 import (
-	"testing"
 	"net"
+	"strconv"
+	"testing"
 )
+
+const distinctNetSetLimit = 42
 
 func makeTestDistinctNetSet() *DistinctNetSet {
 	return &DistinctNetSet{
 		Subnet: 24,
-		Limit:  1,
+		Limit:  distinctNetSetLimit,
 	}
 }
 
 func TestDistinctNetSet(t *testing.T) {
 	set := makeTestDistinctNetSet()
-	testip := net.ParseIP("24.207.212.9")
 
-	key := set.key(testip)
-	t.Logf("key: %v", key)
-
-	if ok := set.Add(testip); !ok {
-		t.Errorf("got: %v, want: %v", ok, true)
-	}
-	if contains := set.Contains(testip); !contains {
-		t.Errorf("got: %v, want: %v", contains, true)
-	}
-	if len := set.Len(); len != 1 {
-		t.Errorf("got: %v, want: %v", len, 1)
+	// 0 <= i <= 252
+	makeTestIp := func(i int) net.IP {
+		return net.ParseIP("24.207.212." + strconv.Itoa(i))
 	}
 
-	set.Remove(testip)
+	var setIps []net.IP
+
+	i := 1
+	for i <= distinctNetSetLimit {
+		testip := makeTestIp(i)
+		key := set.key(testip)
+		t.Logf("ip: %v, key: %v", testip, key)
+
+		if ok := set.Add(testip); !ok {
+			t.Errorf("got: %v, want: %v", ok, true)
+		}
+		if contains := set.Contains(testip); !contains {
+			t.Errorf("got: %v, want: %v", contains, true)
+		}
+		if len := set.Len(); len != uint(i) {
+			t.Errorf("got: %v, want: %v", len, i)
+		}
+		setIps = append(setIps, testip)
+		i++
+	}
+
+	// Show that addition fails once above set limit
+	if i != distinctNetSetLimit+1 {
+		t.Fatalf("i<=distinctNetSetLimit -> got: %v, want: %v", i, distinctNetSetLimit+1)
+	}
+	testip := makeTestIp(i) // i == distinctNetSetLimit + 1
+	if ok := set.Add(testip); ok {
+		t.Errorf("got: %v, want: %v", ok, false)
+	}
 	if contains := set.Contains(testip); contains {
-		t.Errorf("got: %v, want: %v", contains, false)
+		t.Errorf("got: %v, want: %v, testip: %v", contains, false, testip)
 	}
-	if len := set.Len(); len != 0 {
-		t.Errorf("got: %v, want: %v", len, 0)
+	if len := set.Len(); len == uint(i) {
+		t.Errorf("got: %v, want: %v", len, i-1)
+	}
+	i-- // set back to setlimit
+
+	for _, ip := range setIps {
+		set.Remove(ip)
+		i--
+		if contains := set.Contains(ip); contains {
+			t.Errorf("got: %v, want: %v, ip: %v", contains, false, ip)
+		}
+		if len := set.Len(); len != uint(i) {
+			t.Errorf("got: %v, want: %v", len, i)
+		}
 	}
 }
