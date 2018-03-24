9e504c542d7dc1eaed83669b9278659749eddcb9
ia <isaac.ardis@gmail.com>
2018-03-24 02:04:44 -0500

    maybe use bisect properly
    https://stackoverflow.com/a/22592593/4401322

--- hasversion.sh
+++ hasversion.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+echo "building geth..."
 go build -ldflags "-X main.Version=%VERSION%" github.com\ethereumproject\go-ethereum\cmd\geth
+echo "running .\geth version | grep --quiet v (if statement to determine good/bad)..."
 if .\geth version | grep --quiet v5; then
+    echo "OK!!"
 	exit 0
 else
+    echo "FAIL!!"
 	exit 1
 fi
 
