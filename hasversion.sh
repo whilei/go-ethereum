f69ecd8a6286c1748fb4ef53a42b62350e581f0e
ia <isaac.ardis@gmail.com>
2018-03-24 02:15:58 -0500

    maybe fix for windows lingo
    https://stackoverflow.com/questions/11845409/if-then-else-statements-in-windows-batch#11849558

--- hasversion.sh
+++ hasversion.sh
@@ -3,11 +3,11 @@
 echo "building geth..."
 go build -ldflags "-X main.Version=%VERSION%" github.com\ethereumproject\go-ethereum\cmd\geth
 echo "running .\geth version | grep --quiet v (if statement to determine good/bad)..."
-if .\geth version | grep --quiet v5; then
+.\geth version | grep --quiet v (
     echo "OK!!"
 	exit 0
-else
+) || (
     echo "FAIL!!"
 	exit 1
-fi
+)
 
