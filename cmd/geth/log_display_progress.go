41239870c572e5c71c8988b098c14dee57bbad10
ia <isaac.ardis@gmail.com>
2018-06-04 07:49:24 +0200

    problem: no example comment for --display-fmt=progress
    solution: add

--- cmd/geth/log_display_progress.go
+++ cmd/geth/log_display_progress.go
@@ -1,3 +1,37 @@
+/*
+2018-06-04 06:56:41 Geth Classic version: whilei.v5.4.0+143-3916a08[branch=frankenstein/sync-pbs,commit=init-working-basic-progress-bar-display,built=Jun/04Mon-06:56:22]
+2018-06-04 06:56:41 Blockchain: Ethereum Classic Mainnet
+2018-06-04 06:56:41 Chain database: /Users/ia/Library/EthereumClassic/mainnet/chaindata
+2018-06-04 06:56:41 Blockchain upgrades configured: 6
+2018-06-04 06:56:41  1150000 Homestead (0x584bdb5d4e74fe97f5a5222b533fe1322fd0b6ad3eb03f02c3221984e2c0b430)
+2018-06-04 06:56:41  1920000 The DAO Hard Fork (0x94365e3a8c0b35089c1d1195081fe7489b528a84b22199c916180db8b28ade7f)
+2018-06-04 06:56:41  2500000 GasReprice (0xca12c63534f565899681965528d536c52cb05b7c48e269c2a6cb77ad864d878a)
+2018-06-04 06:56:41  3000000 Diehard (0x20a2817ab5545ae3c7aed17e534cffc813040b15e4e095c1eb687b85ff5e5305)
+2018-06-04 06:56:41  5000000 Gotham (0xb2f55d12af971452c3669669380e03040ff01fabb64afec6bfddb3052dbd0117)
+2018-06-04 06:56:41  5900000 Defuse Difficulty Bomb (0x52bc7bbcf1d9251f3f3541ef8c138ee4deacf14e5d66f9614a9bf95d19611bd4)
+2018-06-04 06:56:41 Using 10 configured bootnodes
+2018-06-04 06:56:41 Use Sputnik EVM: true
+2018-06-04 06:56:41 Allotted 4000MB cache and 1024 file handles to /Users/ia/Library/EthereumClassic/mainnet/chaindata
+2018-06-04 06:56:48 Allotted 16MB cache and 16 file handles to /Users/ia/Library/EthereumClassic/mainnet/dapp
+2018-06-04 06:56:48 Protocol Versions: [63 62], Network Id: 1, Chain Id: 61
+2018-06-04 06:56:49 Genesis block: 0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3 (mainnet)
+2018-06-04 06:56:49 Local head header:     #3067492 [0x7d034b…] TD=49543988623626722722
+2018-06-04 06:56:49 Local head full block: #0 [0xd4e567…] TD=17179869184
+2018-06-04 06:56:49 Local head fast block: #3057108 [0x04ac2b…] TD=49395841392101262790
+2018-06-04 06:56:49 Fast sync mode enabled.
+2018-06-04 06:56:49 Starting server...
+2018-06-04 06:56:49 UDP listening. Client enode: enode://1ec5f8dd067172f75ebe404d5e278a118e452a913a17ddfbf102070bb72fefda5d3eb4bceccbdd6a45858b5950dd84723ca55e18e79c7d66d8281aafa1a9d2a5@[::]:30303
+2018-06-04 06:56:49 HTTP endpoint: http://localhost:8545
+2018-06-04 06:56:49 Debug log config: verbosity=0 log-dir=/Users/ia/Library/EthereumClassic/mainnet/log vmodule=*
+2018-06-04 06:56:49 Display log config: display=3 status=sync=1m0s
+2018-06-04 06:56:49 Machine log config: mlog=kv mlog-dir=/Users/ia/Library/EthereumClassic/mainnet/mlogs
+2018-06-04 06:56:49 IPC endpoint opened: /Users/ia/Library/EthereumClassic/mainnet/geth.ipc
+headers  3208371 / 5932806 [******************************************************************************************|----------------------------------------------------------------------------]  54.08%
+receipts 3196404 / 5932806 [*****************************************************************************************|-----------------------------------------------------------------------------]  53.88%
+blocks 0 / 5932806 [-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------]   0.00%
+peers 4 / 50 [============|----------------------------------------------------------------------------------------------------------------------------------------------] -removed 4d3b78723@Parity/v1.10.4
+*/
+
 package main
 
 import (
