9e6005e1e04690d0e7a1744f22c3037a761bc803
ia <isaac.ardis@gmail.com>
2018-06-06 13:04:00 +0200

    crypto/secp256k1: use ethereumproject/ instead of ethereum/ in imports

--- crypto/secp256k1/secp256_test.go
+++ crypto/secp256k1/secp256_test.go
@@ -24,8 +24,8 @@ import (
 	"encoding/hex"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/crypto/randentropy"
+	"github.com/ethereumproject/go-ethereum/common/math"
+	"github.com/ethereumproject/go-ethereum/crypto/randentropy"
 )
 
 const TestCount = 1000
