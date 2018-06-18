875363458451e32ffb93415f1927e9ac306ff359
ia <isaac.ardis@gmail.com>
2018-06-18 21:25:32 +0200

    sed -i 's/ethereum\/go-ethereum/ethereumproject\/go-ethereum/g' ./core/state_transition.go

--- core/state_transition.go
+++ core/state_transition.go
@@ -21,10 +21,10 @@ import (
 	"math"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/ethereumproject/go-ethereum/common"
+	"github.com/ethereumproject/go-ethereum/core/vm"
+	"github.com/ethereumproject/go-ethereum/log"
+	"github.com/ethereumproject/go-ethereum/params"
 )
 
 var (
