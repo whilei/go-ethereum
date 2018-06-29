cf99d33fac9c54739e53b070b649d48bddff7d41
ia <isaac.ardis@gmail.com>
2018-06-29 09:13:14 +0200

    problem: metrics methods missing
    solution: import ./metrics from EF

--- metrics/disk_linux.go
+++ metrics/disk_linux.go
@@ -14,49 +14,59 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+// Contains the Linux implementation of process disk IO counter retrieval.
+
 package metrics
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 	"strconv"
 	"strings"
-
-	"github.com/ethereumproject/go-ethereum/logger/glog"
 )
 
-func readDiskStats(stats *diskStats) {
-	file := fmt.Sprintf("/proc/%d/io", os.Getpid())
-	bytes, err := ioutil.ReadFile(file)
+// ReadDiskStats retrieves the disk IO stats belonging to the current process.
+func ReadDiskStats(stats *DiskStats) error {
+	// Open the process disk IO counter file
+	inf, err := os.Open(fmt.Sprintf("/proc/%d/io", os.Getpid()))
 	if err != nil {
-		glog.Errorf("%s: %s", file, err)
-		return
+		return err
 	}
+	defer inf.Close()
+	in := bufio.NewReader(inf)
 
-	for _, line := range strings.Split(string(bytes), "\n") {
-		i := strings.Index(line, ": ")
-		if i < 0 {
-			continue
-		}
-
-		var p *int64
-		switch line[:i] {
-		case "syscr":
-			p = &stats.ReadCount
-		case "syscw":
-			p = &stats.WriteCount
-		case "rchar":
-			p = &stats.ReadBytes
-		case "wchar":
-			p = &stats.WriteBytes
-		default:
-			continue
-		}
-
-		*p, err = strconv.ParseInt(line[i+2:], 10, 64)
+	// Iterate over the IO counter, and extract what we need
+	for {
+		// Read the next line and split to key and value
+		line, err := in.ReadString('\n')
 		if err != nil {
-			glog.Errorf("%s: line %q: %s", file, line, err)
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+		parts := strings.Split(line, ":")
+		if len(parts) != 2 {
+			continue
+		}
+		key := strings.TrimSpace(parts[0])
+		value, err := strconv.ParseInt(strings.TrimSpace(parts[1]), 10, 64)
+		if err != nil {
+			return err
+		}
+
+		// Update the counter based on the key
+		switch key {
+		case "syscr":
+			stats.ReadCount = value
+		case "syscw":
+			stats.WriteCount = value
+		case "rchar":
+			stats.ReadBytes = value
+		case "wchar":
+			stats.WriteBytes = value
 		}
 	}
 }
--- metricsbak/disk_linux.go
+++ metricsbak/disk_linux.go
@@ -14,13 +14,49 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// +build !linux
-
 package metrics
 
-import "errors"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
 
-// ReadDiskStats retrieves the disk IO stats belonging to the current process.
-func ReadDiskStats(stats *DiskStats) error {
-	return errors.New("Not implemented")
+	"github.com/ethereumproject/go-ethereum/logger/glog"
+)
+
+func readDiskStats(stats *diskStats) {
+	file := fmt.Sprintf("/proc/%d/io", os.Getpid())
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		glog.Errorf("%s: %s", file, err)
+		return
+	}
+
+	for _, line := range strings.Split(string(bytes), "\n") {
+		i := strings.Index(line, ": ")
+		if i < 0 {
+			continue
+		}
+
+		var p *int64
+		switch line[:i] {
+		case "syscr":
+			p = &stats.ReadCount
+		case "syscw":
+			p = &stats.WriteCount
+		case "rchar":
+			p = &stats.ReadBytes
+		case "wchar":
+			p = &stats.WriteBytes
+		default:
+			continue
+		}
+
+		*p, err = strconv.ParseInt(line[i+2:], 10, 64)
+		if err != nil {
+			glog.Errorf("%s: line %q: %s", file, line, err)
+		}
+	}
 }
