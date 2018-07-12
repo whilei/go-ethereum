054b0767fd4b38bd51caa31e96114096d33872c6
ia <isaac.ardis@gmail.com>
2018-07-12 17:41:32 -0400

    runtests.sh script

--- runtests.sh
+++ runtests.sh
@@ -1,12 +1,25 @@
+noteid="$1"
+
 set GOMAXPROCS=2
 
-echo 'go test -v ./... &> gotest.out'
-go test -v ./... &> gotest.out
-echo 'go test -tags=sputnikvm -v ./... &> gotest.svm.out'
-go test -tags=sputnikvm -v ./... &> gotest.svm.out
-echo 'go test -v ./tests/... &> gotest_tests.out'
-go test -v ./tests/... &> gotest_tests.out
-echo 'go test -tags=sputnikvm -v ./tests/... &> gotest_tests.svm.out'
-go test -tags=sputnikvm -v ./tests/... &> gotest_tests.svm.out
+echo $noteid
+
+echo 'go test -v ./... &> gotest.$noteid.out'
+go test -v ./... &> gotest.$noteid.out
+echo 'go test -tags=sputnikvm -v ./... &> gotest.svm.$noteid.out'
+go test -tags=sputnikvm -v ./... &> gotest.svm.$noteid.out
+echo 'go test -v ./tests/... &> gotest_tests.$noteid.out'
+go test -v ./tests/... &> gotest_tests.$noteid.out
+echo 'go test -tags=sputnikvm -v ./tests/... &> gotest_tests.svm.$noteid.out'
+go test -tags=sputnikvm -v ./tests/... &> gotest_tests.svm.$noteid.out
 
 unset GOMAXPROCS
+
+echo 'cat gotest.$noteid.out | grep FAIL | wc -l'
+go test -v ./... &> gotest.$noteid.out
+echo 'go test -tags=sputnikvm -v ./... &> gotest.svm.$noteid.out'
+go test -tags=sputnikvm -v ./... &> gotest.svm.$noteid.out
+echo 'go test -v ./tests/... &> gotest_tests.$noteid.out'
+go test -v ./tests/... &> gotest_tests.$noteid.out
+echo 'go test -tags=sputnikvm -v ./tests/... &> gotest_tests.svm.$noteid.out'
+go test -tags=sputnikvm -v ./tests/... &> gotest_tests.svm.$noteid.out
