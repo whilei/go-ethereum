cd7db6a53c7a7712d0fab5111a8d244fb6b6cf03
ia <isaac.ardis@gmail.com>
2018-10-10 14:42:16 -0500

    Implement unit tests for eip1283storegas calculator fn
    This parses the tests from spec'd table in EIP document, which
    is currently included in the associated JSON tests file

--- core/vm/vm_test.go
+++ core/vm/vm_test.go
@@ -1,62 +1,116 @@
 package vm
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"math/big"
+	"path/filepath"
+	"strconv"
+	"strings"
 	"testing"
 )
 
+type testRuleSet string
+
+type eip1283TestCase struct {
+	index      int
+	code       string
+	wantGas    *big.Int
+	wantRefund *big.Int
+	originalV  *big.Int
+	iters      [3]int64 // potential for 3 possible iterations, -1 will be used when only 2 iterations wanted
+}
+
+func (c eip1283TestCase) String() string {
+	return fmt.Sprintf("i=%d code=%s gas=%v refund=%v original=%v iters=%v", c.index, c.code, c.wantGas, c.wantRefund, c.originalV, c.iters)
+}
+
 func TestEIP1283SStoreGas(t *testing.T) {
-	cases := []struct {
-		wantGas    *big.Int
-		wantRefund *big.Int
-		originalV  *big.Int
-		iters      [3]int64 // potential for 3 possible iterations, -1 will be used when only 2 iterations wanted
-	}{
-		{
-			wantGas:    big.NewInt(412),
-			wantRefund: big.NewInt(0),
-			originalV:  big.NewInt(0),
-			iters:      [3]int64{0, 0, -1},
-		},
-		{
-			wantGas:    big.NewInt(20212),
-			wantRefund: big.NewInt(0),
-			originalV:  big.NewInt(0),
-			iters:      [3]int64{0, 1, -1},
-		},
-		{
-			wantGas:    big.NewInt(20212),
-			wantRefund: big.NewInt(19800),
-			originalV:  big.NewInt(0),
-			iters:      [3]int64{1, 0, -1},
-		},
+	cases := []eip1283TestCase{}
+	eipSpecPath, err := filepath.Abs("../../tests/files/VMTests/ECIP1045/vmEIP1283/eip-1283.md")
+	if err != nil {
+		t.Fatal(err)
+	}
+	readBytes, err := ioutil.ReadFile(eipSpecPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	lines := bytes.Split(readBytes, []byte("\n"))
+	i := 0
+	for _, line := range lines {
+		lineString := string(line)
+		if !strings.HasPrefix(lineString, "|") || !strings.Contains(lineString, "`") || strings.Count(lineString, "|") != 8 {
+			continue
+		}
+		cols := strings.Split(lineString, "|")
+		_code, _gas, _refund, _original, _1st, _2nd, _3rd := cols[1], cols[2], cols[3], cols[4], cols[5], cols[6], cols[7]
+		_code = strings.TrimSpace(_code)
+		_code = strings.Trim(_code, "`")
+
+		_gas = strings.TrimSpace(_gas)
+		_refund = strings.TrimSpace(_refund)
+		_original = strings.TrimSpace(_original)
+		_1st = strings.TrimSpace(_1st)
+		_2nd = strings.TrimSpace(_2nd)
+		_3rd = strings.TrimSpace(_3rd)
+
+		_gasI, _ := strconv.Atoi(_gas)
+		_refundI, _ := strconv.Atoi(_refund)
+		_originalI, _ := strconv.Atoi(_original)
+		_1stI, _ := strconv.Atoi(_1st)
+		_2ndI, _ := strconv.Atoi(_2nd)
+		var _3rdI int
+		if _3rd == "" {
+			_3rdI = -1
+		} else {
+			_3rdI, _ = strconv.Atoi(_3rd)
+		}
+
+		c := eip1283TestCase{
+			index:      i,
+			code:       _code,
+			wantGas:    big.NewInt(int64(_gasI)),
+			wantRefund: big.NewInt(int64(_refundI)),
+			originalV:  big.NewInt(int64(_originalI)),
+			iters:      [3]int64{int64(_1stI), int64(_2ndI), int64(_3rdI)},
+		}
+		cases = append(cases, c)
+		i++
+	}
+
+	if len(cases) < 17 {
+		t.Fatal("unexpected number of cases", len(cases))
 	}
 
 	for i, test := range cases {
 		gotGas := big.NewInt(0)
 		gotRefund := big.NewInt(0)
-		var currentV *big.Int
+		currentV := new(big.Int).Set(test.originalV)
 		for _, iter := range test.iters {
 			if iter == -1 {
 				break
 			}
-			if currentV == nil {
-				currentV = test.originalV
-			}
 			newV := new(big.Int).SetUint64(uint64(iter))
+
 			gas, ref := eip1283sstoreGas(test.originalV, currentV, newV)
+			currentV.Set(newV)
 
 			gotGas.Add(gotGas, gas)
 			gotRefund.Add(gotRefund, ref)
-
-			currentV.Set(newV)
 		}
-		gotGas.Add(gotGas, big.NewInt(12)) // TODO: why is this again?
+
+		// Add gas for PUSH1 ops.
+		pushN := strings.Count(test.code, "60")
+		gotGas.Add(gotGas, new(big.Int).Mul(big.NewInt(int64(pushN)), big.NewInt(3)))
+
 		if gotGas.Cmp(test.wantGas) != 0 {
-			t.Errorf("test: %v; got: %v, want: %v", i+1, gotGas, test.wantGas)
+			t.Log(test)
+			t.Errorf("test: %v; [gas] got=%v, want=%v, diff=%v", i, gotGas, test.wantGas, new(big.Int).Sub(gotGas, test.wantGas))
 		}
 		if gotRefund.Cmp(test.wantRefund) != 0 {
-			t.Errorf("test: %v; got: %v, want: %v", i+1, gotRefund, test.wantRefund)
+			t.Log(test)
+			t.Errorf("test: %v; [refund] got=%v, want=%v, diff=%v", i, gotRefund, test.wantRefund, new(big.Int).Sub(gotRefund, test.wantRefund))
 		}
 	}
 }
