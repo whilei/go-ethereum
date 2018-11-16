f3784d852c5a786b5c1799f3c4e18feb2db658d5
ia <isaac.ardis@gmail.com>
2018-11-15 19:07:50 -0500

    problem: missing metrics method in rawdb
    core/rawdb/schema.go:62:23: undefined: github.com/ethereumproject/go-ethereum/metrics.NewRegisteredCounter
    
    solution: steal ef/metrics

--- metrics/disk.go
+++ metrics/disk.go
@@ -14,13 +14,12 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// +build !linux
-
 package metrics
 
-import "errors"
-
-// ReadDiskStats retrieves the disk IO stats belonging to the current process.
-func ReadDiskStats(stats *DiskStats) error {
-	return errors.New("Not implemented")
+// DiskStats is the per process disk io stats.
+type DiskStats struct {
+	ReadCount  int64 // Number of read operations executed
+	ReadBytes  int64 // Total number of bytes read
+	WriteCount int64 // Number of write operations executed
+	WriteBytes int64 // Total number of byte written
 }
